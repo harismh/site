dfca3e04ccd47e1a9d24c1e01dd9521f8b97995a
Haris Muhammad <hello@harism.dev>
2025-10-19 21:48:29 +0900

    Fix typo in colophon

--- src/main/harism/ui.cljs
+++ src/main/harism/ui.cljs
@@ -179,7 +179,7 @@
                             :cl ["underline" "underline-offset-4"]
                             :c "Replicant."})
                    " Fonts are Untitled Sans from the Klim Foundry
-                   and Plex Sans from IBM. Code is open source at "
+                   and Plex Serif from IBM. Code is open source at "
                    (anchor {:href "https://github.com/harismh/site-v4"
                             :target "_blank"
                             :cl ["underline" "underline-offset-4"]
